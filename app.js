5ca25db794567396618fcdc52dd824a0799bc649
Abdullohtursunov <tursunovabdulloh172@gmail.com>
2024-03-29 02:55:51 +0300

    1

---
+++ app.js
@@ -0,0 +1,61 @@
+//1-masala
+// let setObj = new Set([1, 3, 5, 56, 54, 1, 3, 5]);
+// console.log(setObj);
+// 2-masala
+// let setObj = new Set([1, 1, 5, 5]);
+// let a = 0;
+// setObj.forEach((qiymat) => {
+//   return (a += qiymat);
+// });
+// console.log(a);
+//3-masala
+// let arr = [1, 2, 3, 4, 1];
+// let newsolution = arr.forEach((item) => {
+//   if (item == item) {
+//     console.log(true);
+//   } else {
+//     console.log(false);
+//   }
+// });
+// console.log(newsolution);
+//4-masala
+// let setObj = new Set([1, 3, 5, 56, 54, 1, 3, 5]);
+// let newobj_1 = [...setObj];
+// let setObj_2 = new Set([10, 20, 30, 40]);
+// let newobj_2 = [...setObj_2];
+// let result = newobj_1.concat(newobj_2);
+// console.log(result);
+//5-masala
+// let mapObj = new Map();
+// mapObj.set("strain", "kiyim");
+// mapObj.set("id", 25);
+// mapObj.set("narhi", 3500);
+// console.log(mapObj.size);
+//6-masala
+// let obj = {
+//   id: 521,
+//   title: "iphone 14-pro max",
+//   price: 1050,
+//   color: "grey",
+// };
+// let newobj = {
+//   shop: "house_mobile",
+//   ...obj,
+// };
+// console.log(newobj);
+//7-masala
+//8-masala
+// let setObj = new Set([1, 3, 5, 56, 54, 1, 3, 5]);
+// let newobj_1 = [...setObj];
+// let setObj_2 = new Set([10, 20, 30, 40]);
+// let newobj_2 = [...setObj_2];
+// let result = newobj_1.concat(newobj_2);
+// console.log(result);
+// 9-misol
+// let arr = [1, 2, 3, 4, 5];
+// let a = 0;
+// let newArr = arr.map((item) => {
+//   a += item;
+// });
+// console.log(a);
+//10-masala
